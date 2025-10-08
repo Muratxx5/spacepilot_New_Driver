c2e16fb96acdc9a482e9ea326f69c73f156c4015
Hülya ÖZCAN <44191599+Muratxx5@users.noreply.github.com>
2025-10-08 22:32:53 +0300

    add Buton maps

--- ArduinoSide/fromspacepilottocomputer_V005.ino
+++ ArduinoSide/fromspacepilottocomputer_V005.ino
@@ -8,136 +8,211 @@
 // Then install the 3DxWare software from 3DConnexion on your computer and you can use you Arduino device in software like solidworks, siemens NX, Fusion 360 as it it were a real SpaceMouse.
 // Last update-V005_04/12/2024 - @muratozcan
 #include "HID.h"
- 
+
+// Buton numaralarını SpaceMouse'e özgü düzenle
+#define BUTTON_COUNT 15
+
+/// SpacemouseProWireless Buton eşlemeri
+enum SpaceMouseButtons {
+    SM_MENU     = 0,  // Menu
+    SM_FIT      = 1,  // Fit
+    SM_T        = 2,  // Top
+    SM_R        = 4,  // Right
+    SM_F        = 5,  // Front
+    SM_RCW      = 8,  // Roll 90°CW
+    SM_1        = 12, // 1
+    SM_2        = 13, // 2
+    SM_3        = 14, // 3
+    SM_4        = 15, // 4
+    SM_ESC      = 22, // ESC
+    SM_ALT      = 23, // ALT
+    SM_SHFT     = 24, // SHIFT
+    SM_CTRL     = 25, // CTRL
+    SM_ROT      = 26  // Rotate
+};
+
+
+// 3DConnexion SpaceMouse HID tanımlayıcısı
 static const uint8_t _hidReportDescriptor[] PROGMEM = {
-  0x05, 0x01,           //  Usage Page (Generic Desktop)
-  0x09, 0x08,           //  0x08: Usage (Multi-Axis)
-  0xa1, 0x01,           //  Collection (Application)
-  0xa1, 0x00,           // Collection (Physical)
-  0x85, 0x01,           //  Report ID
-  0x16, 0x00, 0x80,     //logical minimum (-500)
-  0x26, 0xff, 0x7f,     //logical maximum (500)
-  0x36, 0x00, 0x80,     //Physical Minimum (-32768)
-  0x46, 0xff, 0x7f,     //Physical Maximum (32767)
-  0x09, 0x30,           //    Usage (X)
-  0x09, 0x31,           //    Usage (Y)
-  0x09, 0x32,           //    Usage (Z)
-  0x75, 0x10,           //    Report Size (16)
-  0x95, 0x03,           //    Report Count (3)
-  0x81, 0x02,           //    Input (variable,absolute)
-  0xC0,                 //  End Collection
-  0xa1, 0x00,           // Collection (Physical)
-  0x85, 0x02,           //  Report ID
-  0x16, 0x00, 0x80,     //logical minimum (-500)
-  0x26, 0xff, 0x7f,     //logical maximum (500)
-  0x36, 0x00, 0x80,     //Physical Minimum (-32768)
-  0x46, 0xff, 0x7f,     //Physical Maximum (32767)
-  0x09, 0x33,           //    Usage (RX)
-  0x09, 0x34,           //    Usage (RY)
-  0x09, 0x35,           //    Usage (RZ)
-  0x75, 0x10,           //    Report Size (16)
-  0x95, 0x03,           //    Report Count (3)
-  0x81, 0x02,           //    Input (variable,absolute)
-  0xC0,                 //  End Collection
- 
-  0xa1, 0x00,           // Collection (Physical)
-  0x85, 0x03,           //  Report ID
-  0x15, 0x00,           //   Logical Minimum (0)
-  0x25, 0x01,           //    Logical Maximum (1)
-  0x75, 0x01,           //    Report Size (1)
-  0x95, 32,             //    Report Count (24)
-  0x05, 0x09,           //    Usage Page (Button)
-  0x19, 1,              //    Usage Minimum (Button #1)
-  0x29, 32,             //    Usage Maximum (Button #24)
-  0x81, 0x02,           //    Input (variable,absolute)
-  0xC0,
-  0xC0
+    0x05, 0x01,       // Usage Page (Generic Desktop)
+    0x09, 0x08,       // Usage (Multi-Axis)
+    0xA1, 0x01,       // Collection (Application)
+
+    // Report 1: Translation
+    0xa1, 0x00,       // Collection (Physical)
+    0x85, 0x01,       // Report ID (1)
+    0x16, 0x00, 0x80, // Logical Minimum (-32768)
+    0x26, 0xFF, 0x7F, // Logical Maximum (32767)
+    0x36, 0x00, 0x80, // Physical Minimum (-32768)
+    0x46, 0xFF, 0x7F, // Physical Maximum (32767)
+    0x09, 0x30,       // Usage (X)
+    0x09, 0x31,       // Usage (Y)
+    0x09, 0x32,       // Usage (Z)
+    0x75, 0x10,       // Report Size (16)
+    0x95, 0x03,       // Report Count (3)
+    0x81, 0x02,       // Input (Data,Var,Abs)
+    0xC0,             // End Collection
+
+    // Report 2: Rotation
+    0xa1, 0x00,       // Collection (Physical)
+    0x85, 0x02,       // Report ID (2)
+    0x16, 0x00, 0x80, // Logical Minimum (-32768)
+    0x26, 0xFF, 0x7F, // Logical Maximum (32767)
+    0x36, 0x00, 0x80, // Physical Minimum (-32768)
+    0x46, 0xFF, 0x7F, // Physical Maximum (32767)
+    0x09, 0x33,       // Usage (RX)
+    0x09, 0x34,       // Usage (RY)
+    0x09, 0x35,       // Usage (RZ)
+    0x75, 0x10,       // Report Size (16)
+    0x95, 0x03,       // Report Count (3)
+    0x81, 0x02,       // Input (Data,Var,Abs)
+    0xC0,             // End Collection
+
+    // Report 3: Buttons (32 button)
+    0xa1, 0x00,       // Collection (Physical)
+    0x85, 0x03,       // Report ID (3)
+    0x15, 0x00,       // Logical Minimum (0)
+    0x25, 0x01,       // Logical Maximum (1)
+    0x75, 0x01,       // Report Size (1)
+    0x95, 0x20,       // Report Count (32)
+    0x05, 0x09,       // Usage Page (Button)
+    0x19, 0x01,       // Usage Minimum (Button 1)
+    0x29, 0x20,       // Usage Maximum (Button 32)
+    0x81, 0x02,       // Input (Data,Var,Abs)
+    0xC0,             // End Collection
+
+    0xC0              // End Application Collection
 };
- 
-#define DOF 6
-#define TX 0 // translation X
-#define TY 1 // translation Y
-#define TZ 2 // translation Z
-#define RX 3 // rotation X
-#define RY 4 // rotation Y
-#define RZ 5 // rotation Z
- 
-/// hardware 
-#define DEAD_THRESH 2 //Deazone for ignoring small movement
-#define SPEED_PARAM 40 // larger is slower
- 
- 
-// ports of analog input for joysticks
-int port[DOF] = {A0, A2, A6, A1, A3, A7};
- 
-// conversion matrix from sensor input to rigid motion
-int coeff[DOF][DOF] = {
-  { 0,  0,  0,-20,-20, 40}, // TX
-  { 0,  0,  0,-17, 17,  0}, // TY
-  {-3, -3, -3,  0,  0,  0}, // TZ
-  {-6,  6,  0,  0,  0,  0}, // RY
-  { 3,  3, -6,  0,  0,  0}, // RX
-  { 0,  0,  0,  2,  2,  2}, // RZ
-};
- 
-#define abs(x) ((x)<0?(-x):(x))
- 
-int origin[DOF]; // initial sensor values
- 
+
+// Yön ters çevirme ayarları
+bool invX = false;
+bool invY = true;
+bool invZ = true;
+bool invRX = true;
+bool invRY = true;
+bool invRZ = true;
+
+#define BUFFER_SIZE 128
+char serialBuffer[BUFFER_SIZE];
+byte bufferIndex = 0;
+
+uint32_t lastButtonsState = 0;
+
+uint32_t mapButtonToSpaceMouse(uint8_t buttonNumber, uint8_t state) {
+    uint32_t buttons = 0;
+
+    if (state == 0) return 0;
+
+    switch(buttonNumber) {
+        case 0:  break; // Kullanılmayan buton
+       // case 0:  buttons |= (1UL << SM_1); break;
+        case 1:  buttons |= (1UL << SM_1); break; //1
+        case 2:  buttons |= (1UL << SM_2); break; //2
+        case 3:  buttons |= (1UL << SM_3); break; //3
+        case 4:  buttons |= (1UL << SM_4); break; //4
+        case 5:  buttons |= (1UL << SM_ESC); break; //5
+       // case 5:  buttons |= (1UL << SM_4); break;
+      //  case 6:  buttons |= (1UL << SM_T); break;
+        case 6:  buttons |= (1UL << SM_SHFT); break; //6
+        //case 7:  buttons |= (1UL << SM_R); break;
+        case 7:  buttons |= (1UL << SM_T); break;  //T
+        case 8:  buttons |= (1UL << SM_RCW); break;  //L
+        case 9:  buttons |= (1UL << SM_R); break;  //R
+       // case 0:  buttons |= (1UL << SM_MENU); break;
+       // case 1:  buttons |= (1UL << SM_FIT); break;
+       // case 2:  buttons |= (1UL << SM_T); break;
+       // case 4:  buttons |= (1UL << SM_R); break;
+        case 10: buttons |= (1UL << SM_F); break;  //F
+        //case 11: buttons |= (1UL << SM_ESC); break;
+        //case 11: buttons |= (1UL << SM_ESC); break;
+      //  case 12: buttons |= (1UL << SM_ALT); break;
+        //case 13: buttons |= (1UL << SM_SHFT); break;
+        //case 13: buttons |= (1UL << SM_SHFT); break;
+       // case 14: buttons |= (1UL << SM_CTRL); break;
+        case 15: buttons |= (1UL << SM_FIT); break;    //Fit    
+        case 16: buttons |= (1UL << SM_MENU); break;  //Config
+        case 17: buttons |= (1UL << SM_CTRL); break;  //+
+        case 18: buttons |= (1UL << SM_ALT); break;   //-
+        case 19: buttons |= (1UL << SM_ALT); break;  //Doom
+       // case 17: break; // Kullanılmayan buton
+        case 20: buttons |= (1UL << SM_ROT); break;  //3dLock
+        case 21: buttons |= (1UL << SM_MENU); break;  //Panel
+        default: break;
+    }
+
+    return buttons;
+}
+
 void setup() {
-  static HIDSubDescriptor node(_hidReportDescriptor, sizeof(_hidReportDescriptor));
-  HID().AppendDescriptor(&node);
- 
-  for(int i = 0; i < DOF; i++) {
-    origin[i] = analogRead(port[i]);
-  }
+    static HIDSubDescriptor node(_hidReportDescriptor, sizeof(_hidReportDescriptor));
+    HID().AppendDescriptor(&node);
+
+    Serial.begin(115200);
+    memset(serialBuffer, 0, BUFFER_SIZE);
+    delay(1000);
 }
- 
-void send_command(int16_t rx, int16_t ry, int16_t rz, int16_t x, int16_t y, int16_t z) {
-  uint8_t trans[6] = { x & 0xFF, x >> 8, y & 0xFF, y >> 8, z & 0xFF, z >> 8 };
-  HID().SendReport(1, trans, 6);
-  uint8_t rot[6] = { rx & 0xFF, rx >> 8, ry & 0xFF, ry >> 8, rz & 0xFF, rz >> 8 };
-  HID().SendReport(2, rot, 6);
+
+void send_command(int16_t rx, int16_t ry, int16_t rz, int16_t x, int16_t y, int16_t z, uint32_t buttonMask = 0) {
+    if (invX) x = -x;
+    if (invY) y = -y;
+    if (invZ) z = -z;
+    if (invRX) rx = -rx;
+    if (invRY) ry = -ry;
+    if (invRZ) rz = -rz;
+
+    int16_t trans_data[3] = {x, y, z};
+    HID().SendReport(1, trans_data, sizeof(trans_data));
+
+    int16_t rot_data[3] = {rx, ry, rz};
+    HID().SendReport(2, rot_data, sizeof(rot_data));
+
+    HID().SendReport(3, &buttonMask, sizeof(buttonMask));
 }
- 
+
 void loop() {
-  int sv[DOF]; // sensor value
-  int mv[DOF]; // motion vector
-  
-  // read sensor value and subtract original position
-  for(int i = 0; i < DOF; i++) {
-    sv[i] = analogRead(port[i]) - origin[i];
-  }
- 
-  // calculate the motion of the "mushroom" knob
-  for(int i = 0; i < DOF; i++) {
-    mv[i] = 0;
-    for(int j = 0; j < DOF; j++) {
-      mv[i] += coeff[i][j] * sv[j];
+    while (Serial.available() > 0) {
+        char incomingChar = Serial.read();
+
+        if (incomingChar != '\n') {
+            if (bufferIndex < BUFFER_SIZE - 1) {
+                serialBuffer[bufferIndex++] = incomingChar;
+            }
+        } else {
+            serialBuffer[bufferIndex] = '\0';
+            processReceivedData(serialBuffer);
+            bufferIndex = 0;
+        }
     }
-    
-    mv[i] /= SPEED_PARAM;
-    
-    if((mv[i] > -DEAD_THRESH) && (mv[i] < DEAD_THRESH)){
-      mv[i] = 0;
+}
+
+void processReceivedData(char* data) {
+    char* token = strtok(data, ",");
+    if (token == NULL) return;
+    int16_t rotX = atoi(token);
+
+    token = strtok(NULL, ","); if (token == NULL) return;
+    int16_t rotY = atoi(token);
+
+    token = strtok(NULL, ","); if (token == NULL) return;
+    int16_t rotZ = atoi(token);
+
+    token = strtok(NULL, ","); if (token == NULL) return;
+    int16_t transX = atoi(token);
+
+    token = strtok(NULL, ","); if (token == NULL) return;
+    int16_t transY = atoi(token);
+
+    token = strtok(NULL, ","); if (token == NULL) return;
+    int16_t transZ = atoi(token);
+
+    uint32_t buttonMask = 0;
+    while ((token = strtok(NULL, ",")) != NULL) {
+        uint8_t key = atoi(token);
+        token = strtok(NULL, ",");
+        if (token == NULL) break;
+        uint8_t state = atoi(token);
+        buttonMask |= mapButtonToSpaceMouse(key, state);
     }
-    
-    else if(mv[i] > 500) {
-      mv[i] = 500;
-    }
-    else if(mv[i] < -500) {
-      mv[i] = -500;
-    }
-  }
- 
-  bool Movement = false;
-  for(int i = 0; i < DOF; i++) {
-    if(mv[i] != 0){
-      Movement = true;
-    }
-  }
- 
-  if(Movement = true){
-    send_command(mv[4], -mv[3], -mv[5], mv[0], mv[1], mv[2]);
-  }
+
+    send_command(rotX, rotY, rotZ, transX, transY, transZ, buttonMask);
 }
